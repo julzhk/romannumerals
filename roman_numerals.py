3980a4a8ff840d956573cb1570d3b68e47ec1c98
Julian Harley <julian@bynd.com>
2012-12-04 20:02:16 +0000

    initial commit

---
+++ roman_numerals.py
@@ -0,0 +1,15 @@
+import  unittest
+
+class TestRomanNumerals(unittest.TestCase):
+    def setUp(self):
+        pass
+
+def convert_roman_to_decimal(roman):
+    return 6
+
+    def test_test(self):
+        self.assertEqual(convert_roman_to_decimal(6),6)
+
+
+if __name__ == '__main__':
+    unittest.main()
