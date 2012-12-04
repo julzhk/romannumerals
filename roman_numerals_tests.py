223ab7a1c9149d98115c4d6d0519a870286dda61
Julian Harley <julian@bynd.com>
2012-12-04 21:04:12 +0000

    sketches towards an addition function

--- roman_numerals_tests.py
+++ roman_numerals_tests.py
@@ -1,6 +1,23 @@
 import  unittest
 from romcalc import RomanNumeral
 
+degenerate_roman_lookup = {
+    'IIII':'IV',
+    'IVI':'V',
+    'VIII': 'IX',
+    'IXI': 'X'
+    }
+
+def simplify(x):
+    while True:
+        for complicated, simple in degenerate_roman_lookup.items():
+            if x.startswith(complicated):
+                rest = complicated[len(simple):]
+                x = simple + rest
+                break
+        else:
+            return x
+
 class TestRomanNumerals(unittest.TestCase):
 
     def test_romans(self):
@@ -16,6 +33,9 @@ class TestRomanNumerals(unittest.TestCase):
     def test_find_initial_digits(self):
         self.assertEqual(RomanNumeral(449),'CDXLIX')
         self.assertEqual(RomanNumeral(299),'CCXCIX')
+
+    def test_simplify(self):
+        self.assertEqual(simplify('IIII'), 'IV')
 
 
 if __name__ == '__main__':
